f60550f4d64233bd84dc7bcaa9241b93cc2aaaee
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-07 10:27:07 +0530

    complete problem 'Groundhogs at a party'.

--- Problems/Groundhogs at a party/src/Main.java
+++ Problems/Groundhogs at a party/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        int num = scanner.nextInt();
+        boolean weekend = scanner.nextBoolean();
+        System.out.println(num >= 10 && num <= 20 && !weekend || num >= 15 && num <= 25 && weekend);
     }
 }
