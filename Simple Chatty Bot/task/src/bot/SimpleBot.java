6199087d0a464a6eb97c1c04d899a72778aec4b0
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2020-06-07 10:25:28 +0530

    complete problem 'Groundhogs at a party'.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -16,11 +16,17 @@ public class SimpleBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        int remOf3 = scanner.nextInt();
-        int remOf5 = scanner.nextInt();
-        int remOf7 = scanner.nextInt();
-        int age = (remOf3 * 70 + remOf5 * 21 + remOf7 * 15) % 105;
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+
+        System.out.println("Completed, have a nice day!");
     }
 }
